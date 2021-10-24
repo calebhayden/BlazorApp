0044165decc96def12a3ac013e30945bb248b9ea
Caleb Hayden <caleb@brickbridgeconsulting.com>
2021-10-23 22:03:12 -0400

    css fiddling; Responsive buttons

--- Pages/Retro/RetroItem.cs
+++ Pages/Retro/RetroItem.cs
@@ -2,9 +2,12 @@ public class RetroItem
 {
     public int Id { get; set; }
     public string Text { get; set; }
-    public int Likes { get; set; }
-    public int Dislikes { get; set; }
+    public bool IsLiked { get; set; }
+    public bool IsDisliked { get; set; }
     public bool IsStarred { get; set; }
-    public string Author {get; set;}
+    public string Author {get; set; }
+    public string LikeImgSrc => IsLiked ? "../Icons/thumbs-up-filled.svg" : "../Icons/thumbs-up.svg";
+    public string DislikeImgSrc => IsDisliked ? "../Icons/thumbs-down-filled.svg" : "../Icons/thumbs-down.svg";
+    public string StarImgSrc => IsStarred ? "../Icons/star-filled.svg" : "../Icons/star.svg";
 
 }
